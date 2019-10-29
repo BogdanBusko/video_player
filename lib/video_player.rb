2fb81242e16475068df1cc2e81c82d0a5f94e9d4
Busko Bogdan <busko.bogdan@gmail.com>
2019-10-29 10:49:21 +0200

    Update configurations

--- lib/video_player.rb
+++ lib/video_player.rb
@@ -6,49 +6,46 @@ module VideoPlayer
   end
 
   class Parser
-    DefaultWidth = '420'
-    DefaultHeight = '315'
-    DefaultAutoPlay = true
+    YOUTUBE_REGEX  = /\A(https?:\/\/)?(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/i
+    VIMEO_REGEX    = /\Ahttps?:\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/i
+    IZLESENE_REGEX = /\Ahttp:\/\/(?:.*?)\izlesene\.com\/video\/([\w\-\.]+[^#?\s]+)\/(.*)?$/i
+    WISTIA_REGEX   = /\Ahttps?:\/\/(.+)?(wistia.com|wi.st)\/(medias|embed)\/([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/i
 
-    YouTubeRegex  = /\A(https?:\/\/)?(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/i
-    VimeoRegex    = /\Ahttps?:\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/i
-    IzleseneRegex = /\Ahttp:\/\/(?:.*?)\izlesene\.com\/video\/([\w\-\.]+[^#?\s]+)\/(.*)?$/i
-    WistiaRegex   = /\Ahttps?:\/\/(.+)?(wistia.com|wi.st)\/(medias|embed)\/([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/i
+    attr_accessor :url, :width, :height, :allow_full_screen
 
-    attr_accessor :url, :width, :height
-
-    def initialize(url, width = DefaultWidth, height = DefaultHeight, autoplay = DefaultAutoPlay)
+    def initialize(url, width: 440, height: 200, autoplay: true, allow_full_screen: true, show_controls: true)
       @url = url
       @width = width
       @height = height
       @autoplay = autoplay
+      @allow_full_screen = allow_full_screen
+      @show_controls = show_controls
     end
 
     def embed_code
       case
-      when matchdata = url.match(YouTubeRegex)
+      when matchdata = url.match(YOUTUBE_REGEX)
         youtube_embed(matchdata[4])
-      when matchdata = url.match(VimeoRegex)
+      when matchdata = url.match(VIMEO_REGEX)
         vimeo_embed(matchdata[2])
-      when matchdata = url.match(IzleseneRegex)
+      when matchdata = url.match(IZLESENE_REGEX)
         izlesene_embed(matchdata[2])
-      when matchdata = url.match(WistiaRegex)
+      when matchdata = url.match(WISTIA_REGEX)
         wistia_embed(matchdata[4])
       else
         false
       end
     end
 
-    def autoplay
-      !!@autoplay ? '1' : '0'
-    end
 
     def iframe_code(src)
-      %{<iframe src="#{src}" width="#{width}" height="#{height}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>}
+      full_screen = allow_full_screen ? 'webkitallowfullscreen mozallowfullscreen allowfullscreen' : ''
+
+      "<iframe src=\"#{src}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"0\" #{full_screen}></iframe>"
     end
 
     def youtube_embed(video_id)
-      src = "//www.youtube.com/embed/#{video_id}?autoplay=#{autoplay}&rel=0"
+      src = "//www.youtube.com/embed/#{video_id}?autoplay=#{autoplay}&controls=#{controls}&disablekb=#{controls}&rel=0"
       iframe_code(src)
     end
 
@@ -65,6 +62,16 @@ module VideoPlayer
     def wistia_embed(video_id)
       src = "//fast.wistia.net/embed/iframe/#{video_id}/?autoplay=#{autoplay}&showrel=0&showinfo=0"
       iframe_code(src)
+    end
+
+    private
+
+    def autoplay
+      autoplay ? '1' : '0'
+    end
+
+    def controls
+      show_controls ? '1' : '0'
     end
   end
 end
